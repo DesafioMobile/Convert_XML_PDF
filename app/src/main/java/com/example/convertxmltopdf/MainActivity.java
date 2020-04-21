e8882ecbf31a5ae84537a165df9fcea374fff226
Júlio Alves <julio619alves@gmail.com>
2020-04-21 19:26:21 -0300

    rename result file

--- app/src/main/java/com/example/convertxmltopdf/MainActivity.java
+++ app/src/main/java/com/example/convertxmltopdf/MainActivity.java
@@ -14,12 +14,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/convertxmltopdf/ResultActivity.java
+++ app/src/main/java/com/example/convertxmltopdf/ResultActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class xmlresult extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_xmlresult);
+        setContentView(R.layout.activity_result);
     }
 }
